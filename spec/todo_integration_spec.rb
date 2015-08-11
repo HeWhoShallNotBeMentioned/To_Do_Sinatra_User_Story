f2b3d97fce7c5fc4c5b01e1131b8ae193aab85f5
Austin Kincaid & Lindsay Culver <student@epicodus.com>
2015-08-11 10:30:14 -0700

    Add integration spec to confirm return from success page after new task entry

--- spec/todo_integration_spec.rb
+++ spec/todo_integration_spec.rb
@@ -10,4 +10,13 @@ describe('todo list execution path', {:type => :feature}) do
     click_button('Add Task')
     expect(page).to have_content('The task has been submitted!')
   end
+
+  it('takes a todo item and gives a success message') do
+    visit('/')
+    fill_in('description', :with => 'commit the files')
+    click_button('Add Task')
+    expect(page).to have_content('The task has been submitted!')
+    click_link_or_button('Back')
+    expect(page).to have_content('commit the files')
   end
+end
