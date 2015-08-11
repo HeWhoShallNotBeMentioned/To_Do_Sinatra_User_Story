8911f8d4d55de99162c4dab3a986d24075bc3f24
Austin Kincaid & Lindsay Culver <student@epicodus.com>
2015-08-11 09:26:31 -0700

    Add spec and method to save new tasks

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -8,4 +8,18 @@ describe(Todo) do
       expect(test_task.description()).to(eq("commit the code"))
     end
   end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Todo.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Todo.new("write new spec")
+      test_task.save()
+      expect(Todo.all()).to(eq([test_task]))
+    end
+  end
 end
