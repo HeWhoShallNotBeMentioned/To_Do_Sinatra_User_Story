cc21d10fa9c5ecca24dce4e48a5c9b62037f0c76
Austin Kincaid & Chris Underwood <student@epicodus.com>
2015-08-17 11:51:55 -0700

    modify the lib and spec to_do files to talk to lists.

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,8 +1,9 @@
 class Todo
-  attr_reader(:description)
+  attr_reader(:description, :list_id)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
+    @list_id = attributes.fetch(:list_id)
   end
 
   define_singleton_method(:all) do
@@ -10,20 +11,17 @@ class Todo
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
-      tasks.push(Todo.new({:description => description}))
+      list_id = task.fetch("list_id").to_i()
+      tasks.push(Todo.new({:description => description, :list_id => list_id}))
     end
     tasks
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
   end
 
-  # define_singleton_method(:clear) do
-  #   @@all_tasks = []
-  # end
-
   define_method(:==) do |another_task|
-    self.description().==(another_task.description())
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
   end
 end
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -11,21 +11,6 @@ RSpec.configure do |config|
 end
 
 describe(Todo) do
-  describe("#==") do
-    it("is the same task if it has the same description") do
-      todo1 = Todo.new({:description => "learn SQL"})
-      todo2 = Todo.new({:description => "learn SQL"})
-      expect(todo1).to(eq(todo2))
-    end
-  end
-
-  describe("#description") do
-    it("lets you give a description") do
-      test_task = Todo.new({:description => "commit the code"})
-      expect(test_task.description()).to(eq("commit the code"))
-    end
-  end
-
   describe(".all") do
     it("is empty at first") do
       expect(Todo.all()).to(eq([]))
@@ -34,17 +19,31 @@ describe(Todo) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Todo.new({:description => "write new spec"})
+      test_task = Todo.new({:description => "write new spec", :list_id => 1})
       test_task.save()
       expect(Todo.all()).to(eq([test_task]))
     end
   end
 
-  # describe(".clear") do
-  #   it("empties out all of the saved tasks") do
-  #     Todo.new({:description => "Green onions please."}).save()
-  #     Todo.clear()
-  #     expect(Todo.all()).to(eq([]))
-  #   end
-  # end
+  describe("#description") do
+    it("lets you give a description") do
+      test_task = Todo.new({:description => "commit the code", :list_id => 1})
+      expect(test_task.description()).to(eq("commit the code"))
+    end
+  end
+
+  describe('#list_id') do
+    it('lets you read the list ID out') do
+      test_task = Todo.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      todo1 = Todo.new({:description => "learn SQL", :list_id => 1})
+      todo2 = Todo.new({:description => "learn SQL", :list_id => 1})
+      expect(todo1).to(eq(todo2))
+    end
+  end
 end
