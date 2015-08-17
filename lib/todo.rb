19e45be45a75771a52043ae161d42ffa3c0b8d39
Austin Kincaid & Chris Underwood <student@epicodus.com>
2015-08-17 13:51:50 -0700

    Add spec and ability to return database items by most pressing

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,27 +1,29 @@
 class Todo
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :list_id, :due_date)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @due_date = attributes.fetch(:due_date)
   end
 
   define_singleton_method(:all) do
-    returned_tasks = DB.exec("Select * FROM tasks;")
+    returned_tasks = DB.exec("Select * FROM tasks ORDER BY due_date ASC;")
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
       list_id = task.fetch("list_id").to_i()
-      tasks.push(Todo.new({:description => description, :list_id => list_id}))
+      due_date = task.fetch("due_date")
+      tasks.push(Todo.new({:description => description, :list_id => list_id, :due_date => due_date}))
     end
     tasks
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, list_id, due_date) VALUES ('#{@description}', #{@list_id}, '#{@due_date}');")
   end
 
   define_method(:==) do |another_task|
-    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id())).&(self.due_date().==(another_task.due_date()))
   end
 end
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -5,11 +5,21 @@ describe(Todo) do
     it("is empty at first") do
       expect(Todo.all()).to(eq([]))
     end
+
+    it('returns items in order of due_date; the closest by date being presented first') do
+      test_task = Todo.new({:description => "write new spec", :list_id => 1, :due_date => "2016-02-14 00:00:00"})
+      test_task.save()
+      test_task2 = Todo.new({:description => "write new spec", :list_id => 1, :due_date => "2016-02-13 00:00:00"})
+      test_task2.save()
+      test_task3 = Todo.new({:description => "write new spec", :list_id => 1, :due_date => "2016-02-15 00:00:00"})
+      test_task3.save()
+      expect(Todo.all()).to(eq([test_task2, test_task, test_task3]))
+    end
   end
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Todo.new({:description => "write new spec", :list_id => 1})
+      test_task = Todo.new({:description => "write new spec", :list_id => 1, :due_date => "2015-02-14 00:00:00"})
       test_task.save()
       expect(Todo.all()).to(eq([test_task]))
     end
@@ -17,22 +27,22 @@ describe(Todo) do
 
   describe("#description") do
     it("lets you give a description") do
-      test_task = Todo.new({:description => "commit the code", :list_id => 1})
+      test_task = Todo.new({:description => "commit the code", :list_id => 1, :due_date => "2015-02-14 00:00:00"})
       expect(test_task.description()).to(eq("commit the code"))
     end
   end
 
   describe('#list_id') do
     it('lets you read the list ID out') do
-      test_task = Todo.new({:description => "learn SQL", :list_id => 1})
+      test_task = Todo.new({:description => "learn SQL", :list_id => 1,:due_date => "2015-02-14 00:00:00"})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
   describe("#==") do
     it("is the same task if it has the same description") do
-      todo1 = Todo.new({:description => "learn SQL", :list_id => 1})
-      todo2 = Todo.new({:description => "learn SQL", :list_id => 1})
+      todo1 = Todo.new({:description => "learn SQL", :list_id => 1, :due_date => "2015-02-14 00:00:00"})
+      todo2 = Todo.new({:description => "learn SQL", :list_id => 1, :due_date => "2015-02-14 00:00:00"})
       expect(todo1).to(eq(todo2))
     end
   end
