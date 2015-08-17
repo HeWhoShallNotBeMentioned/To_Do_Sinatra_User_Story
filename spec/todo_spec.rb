f32e9437cb1f3773e31b47220a5f2baca8aaa32e
Austin Kincaid & Chris Underwood <student@epicodus.com>
2015-08-17 12:00:38 -0700

    add spec_helper.rb file along with links to list_spec and todo_spec

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,14 +1,4 @@
-require('rspec')
-require('pg')
-require('todo')
-
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
+require('spec_helper')
 
 describe(Todo) do
   describe(".all") do
