54ca2cb29b1f8ba36688597082940e5b128eae9a
Austin Kincaid & Chris Underwood <student@epicodus.com>
2015-08-17 11:00:58 -0700

    add list_spec.rb with full spec(untested) modified app.rb to uncomment DB, modified todo.rb to add in database methods

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -34,18 +34,17 @@ describe(Todo) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Todo.new("write new spec")
+      test_task = Todo.new({:description => "write new spec"})
       test_task.save()
       expect(Todo.all()).to(eq([test_task]))
     end
   end
 
-  describe(".clear") do
-    it("empties out all of the saved tasks") do
-      Todo.new("Green onions please.").save()
-      Todo.clear()
-      expect(Todo.all()).to(eq([]))
-    end
-  end
-
+  # describe(".clear") do
+  #   it("empties out all of the saved tasks") do
+  #     Todo.new({:description => "Green onions please."}).save()
+  #     Todo.clear()
+  #     expect(Todo.all()).to(eq([]))
+  #   end
+  # end
 end
