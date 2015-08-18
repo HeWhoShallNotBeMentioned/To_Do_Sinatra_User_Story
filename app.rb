1006506497c8ee8d4377d82c2133cfb0f27d3611
Chris Underwood & Jing Wait <student@epicodus.com>
2015-08-18 10:15:29 -0700

    add edits for the app.rb along with index and success erb views to accommodate lists

--- app.rb
+++ app.rb
@@ -1,19 +1,32 @@
 require("sinatra")
 require("sinatra/reloader")
-also_reload("lib/**/*.rb")
 require("./lib/todo")
+require("./lib/list")
+also_reload("lib/**/*.rb")
 require('pg')
+
 
 DB = PG.connect({:dbname => "to_do"})
 
 get("/") do
-  @tasks = Todo.all()
+  #@tasks = Todo.all()
   erb(:index)
 end
 
-post("/tasks") do
-  description = params.fetch("description")
-  task = Todo.new(description)
-  task.save()
-  erb(:success)
+get("/lists/new") do
+  erb:(:list_form)
 end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
+end
+
+# post("/tasks") do
+#   description = params.fetch("description")
+#   task = Todo.new(description)
+#   task.save()
+#   erb(:success)
+# end
